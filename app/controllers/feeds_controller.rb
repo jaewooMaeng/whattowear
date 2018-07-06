0d127678710f7f672ca8d39b32ac2167cf9b5d3d
qwerty98765 <maeng9584@naver.com>
2018-07-06 19:34:31 +0900

    structure

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,38 @@
 class FeedsController < ApplicationController
+    def index
+        @feeds = Feed.all
+        render 'index'
+    end
+    def board
+        render 'board'  
+    end
+    def new
+        render 'new'
+    end
+    def mycloset
+        render 'mycloset'
+    end
+    def studiostreet
+        render 'studiostreet'
+    end
+    def editor
+        render 'editor'
+    end
+    def qna
+        render 'qna'
+    end
+
+
+
+
+
+    def create
+        title = params[:title]
+        content = params[:content]
+        feed = Feed.new
+        feed.title = title
+        feed.content = content
+        feed.save
+        redirect_to action: 'index'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'feeds#index'
-
+  get '/feeds', to: 'feeds#index'
+  get '/board', to: 'feeds#board'
+  get '/feeds/new', to: 'feeds#new'
+  post '/feeds', to: 'feeds#create'
+  get '/feeds/mycloset', to: 'feeds#mycloset'
+  get '/feeds/studiostreet', to: 'feeds#studiostreet'
+  get '/feeds/editor', to: 'feeds#editor'
+  get '/feeds/qna', to: 'feeds#qna'
 end
